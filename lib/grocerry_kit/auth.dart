3a5b89d2718f5d1e6bc3b0f43b9d113cc4c2ff87
Leo Elstin <leo.elstin@azinova.info>
2019-04-25 19:44:04 +0530

    fixes

--- lib/grocerry_kit/auth.dart
+++ lib/grocerry_kit/auth.dart
@@ -14,26 +14,26 @@ class _AuthPageState extends State<AuthPage> {
       body: Container(
         height: 350,
         decoration: BoxDecoration(
-           boxShadow: [
-            new BoxShadow(
-              color: Colors.grey,
-              offset: new Offset(0.0, 5.0),
-              blurRadius: 25.0,
-            )
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(32),
-            bottomRight: Radius.circular(32)
-          )
-        ),
+            boxShadow: [
+              new BoxShadow(
+                color: Colors.grey,
+                offset: new Offset(0.0, 5.0),
+                blurRadius: 25.0,
+              )
+            ],
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(32),
+                bottomRight: Radius.circular(32))),
         alignment: Alignment.topCenter,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Container(
                   margin: EdgeInsets.all(32),
@@ -62,6 +62,20 @@ class _AuthPageState extends State<AuthPage> {
                   ),
                 ),
               ],
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 16, top: 16),
+              child: Text(
+                'Welcome to keells.',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                'Let\'s get started',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+              ),
             )
           ],
         ),
