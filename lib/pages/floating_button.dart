d1d7b8daa0aa6fd702d40e0ed62cc461ee2e13c0
Leo Elstin <leoelstin@gmail.com>
2019-04-27 13:01:01 +0530

    fixes

--- lib/pages/floating_button.dart
+++ lib/pages/floating_button.dart
@@ -31,37 +31,55 @@ class _FloatingPageState extends State<FloatingPage>
       ),
       floatingActionButton: Column(
 //        crossAxisAlignment: CrossAxisAlignment.baseline,
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           ScaleTransition(
-            scale: CurvedAnimation(
-                parent: _controller,
-                curve: Interval(0.0, 1.0, curve: Curves.easeOut)),
-            child: Container(
-              margin: EdgeInsets.all(8),
-              child: FloatingActionButton(
-                onPressed: null,
-                backgroundColor: Colors.white,
-                mini: true,
-                child: Icon(Icons.phone, color: Colors.blue,),
-              ),
-            ),
-          ),
+              scale: CurvedAnimation(
+                  parent: _controller,
+                  curve: Interval(0.0, 1.0, curve: Curves.easeOut)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Inborwrerx'),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    child: FloatingActionButton(
+                      onPressed: null,
+                      backgroundColor: Colors.white,
+                      mini: true,
+                      child: Icon(
+                        Icons.email,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ],
+              )),
           ScaleTransition(
-            scale: CurvedAnimation(
-                parent: _controller,
-                curve: Interval(0.0, 0.5, curve: Curves.easeOut)),
-            child: Container(
-              margin: EdgeInsets.all(8),
-              child: FloatingActionButton(
-                onPressed: null,
-                backgroundColor: Colors.white,
-                mini: true,
-                child: Icon(Icons.email, color: Colors.red,),
-              ),
-            ),
-          ),
+              scale: CurvedAnimation(
+                  parent: _controller,
+                  curve: Interval(0.0, 0.5, curve: Curves.easeOut)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Inborwrerx'),
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    child: FloatingActionButton(
+                      onPressed: null,
+                      backgroundColor: Colors.white,
+                      mini: true,
+                      child: Icon(
+                        Icons.email,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ],
+              )),
           Container(
             margin: EdgeInsets.all(8),
             child: FloatingActionButton(
@@ -78,8 +96,10 @@ class _FloatingPageState extends State<FloatingPage>
                     builder: (context, widget) {
                       return Transform(
                         alignment: FractionalOffset.center,
-                        transform: Matrix4.rotationZ(_controller.value * 1 * math.pi),
-                        child: Icon( _controller.isDismissed? Icons.edit : Icons.close),
+                        transform:
+                            Matrix4.rotationZ(_controller.value * 1 * math.pi),
+                        child: Icon(
+                            _controller.isDismissed ? Icons.edit : Icons.close),
                       );
                     })),
           ),
