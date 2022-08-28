bd09b37beaeb557e0b02fe907166c0b4e998d69e
leo-elstin <leo.e@maersk.com>
2022-08-28 11:12:58 +0530

    twitter clone initial

---
+++ shopping_app/lib/view/home/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ twitter_clone/android/app/src/main/kotlin/dev/leyo/twitter_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.leyo.twitter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
